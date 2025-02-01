14276972ebf96102d7135cf9e59e85f1fe69b55d
MysticDirt <70498912+MysticDirt@users.noreply.github.com>
2025-02-01 12:13:08 -0800

    Update portsToProcess.ps1

--- windows/portsToProcess.ps1
+++ windows/portsToProcess.ps1
@@ -37,43 +37,4 @@ Get-NetUDPEndpoint | Select-Object LocalPort, OwningProcess,
         }
     }} | Format-Table -AutoSize -Wrap
 
-Write-Host "TCP Connections Listening" -ForegroundColor Green
-Get-NetTCPConnection -State Listen | Select-Object LocalPort, OwningProcess, 
-    @{Name='ProcessName';Expression={(Get-Process -Id $_.OwningProcess).Name}}, 
-    @{Name='Services';Expression={
-        $processId = $_.OwningProcess
-        $procName = (Get-Process -Id $processId).Name
-        if ($procName -eq 'svchost') {
-            (Get-CimInstance Win32_Service | Where-Object { $_.ProcessId -eq $processId } | Select-Object -ExpandProperty Name) -join ', '
-        } else {
-            $null
-        }
-    }} | Format-Table -AutoSize -Wrap
-
-Write-Host "TCP Connections Established" -ForegroundColor Green
-Get-NetTCPConnection -State Established | Select-Object LocalPort, OwningProcess, 
-    @{Name='ProcessName';Expression={(Get-Process -Id $_.OwningProcess).Name}}, 
-    @{Name='Services';Expression={
-        $processId = $_.OwningProcess
-        $procName = (Get-Process -Id $processId).Name
-        if ($procName -eq 'svchost') {
-            (Get-CimInstance Win32_Service | Where-Object { $_.ProcessId -eq $processId } | Select-Object -ExpandProperty Name) -join ', '
-        } else {
-            $null
-        }
-    }} | Format-Table -AutoSize -Wrap
-
-Write-Host "UDP Endpoints" -ForegroundColor Green
-Get-NetUDPEndpoint | Select-Object LocalPort, OwningProcess, 
-    @{Name='ProcessName';Expression={(Get-Process -Id $_.OwningProcess).Name}}, 
-    @{Name='Services';Expression={
-        $processId = $_.OwningProcess
-        $procName = (Get-Process -Id $processId).Name
-        if ($procName -eq 'svchost') {
-            (Get-CimInstance Win32_Service | Where-Object { $_.ProcessId -eq $processId } | Select-Object -ExpandProperty Name) -join ', '
-        } else {
-            $null
-        }
-    }} | Format-Table -AutoSize -Wrap
-
 Read-Host -Prompt "Press Enter to exit"
