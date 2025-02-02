40221d413d2d0c1f47a917f6cb1754408c2d10ec
NuclearFizzler <63215105+NuclearFizzler@users.noreply.github.com>
2025-02-01 18:10:52 -0800

    Added anti-lockout measure on DC script

--- windows/DC_deploy.ps1
+++ windows/DC_deploy.ps1
@@ -8,7 +8,7 @@ $currentPrincipal = New-Object Security.Principal.WindowsPrincipal( [Security.Pr
 & {
     if ($currentPrincipal.IsInRole( [Security.Principal.WindowsBuiltInRole]::Administrator ))
     {
-        Write-Output "Oppenheimer mode engage"
+        Write-Host "Oppenheimer mode engage"
     }
     else {
         $Location2 = $PSCommandPath
@@ -32,10 +32,9 @@ $userList = @()
 	
 	$api = Invoke-RestMethod -Uri "https://api.genratr.com/?special&lowercase&uppercase&numbers" -Method Get
 	$p = ConvertTo-SecureString -String $api.password -AsPlainText -Force
-	set-adaccountpassword -identity $psitem -newpassword $p
+	Set-ADAccountPassword -identity $psitem -newpassword $p
 
     
-
     $userList += [PSCustomObject]@{username = $username; password = $api.password}
 }
 
@@ -43,6 +42,9 @@ $userList = @()
 # Generate .csv for password changes
 $userList | Export-Csv -Path ".\password.csv" -NoTypeInformation
 
+Write-Host "Reset the password for Administrator so you don't get locked out"
+$p = Read-Host -AsSecureString
+Set-ADAccountPassword -identity Administrator -newpassword $p
 
 # Enable firewall
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled True
