fe1ad16e1f536a070ba83bc7d41dec2b443ba22b
Tyler C <taculp@ucdavis.edu>
2025-03-09 20:11:16 -0700

    force script to just run as sudo

--- linux/sop/linux_sop.sh
+++ linux/sop/linux_sop.sh
@@ -50,13 +50,13 @@ getMachineInfo() {
 getRunningServices() {
     mkdir -p ~/sop/running
 
-    sudo systemctl --type=service --state=running >> ~/sop/running/runningServices.txt
-    sudo systemctl list-unit-files --state=enabled >> ~/sop/running/enabledServices.txt
+    systemctl --type=service --state=running >> ~/sop/running/runningServices.txt
+    systemctl list-unit-files --state=enabled >> ~/sop/running/enabledServices.txt
 
-    sudo ss -plnt >> ~/sop/running/openPorts.txt
-    sudo ss -plnu >> ~/sop/running/openPorts.txt
+    ss -plnt >> ~/sop/running/openPorts.txt
+    ss -plnu >> ~/sop/running/openPorts.txt
 
-    sudo nmap -p- localhost >> ~/sop/running/localNmapScan.txt
+    nmap -p- localhost >> ~/sop/running/localNmapScan.txt
 }
 
 sshConfigSetUp() {
@@ -73,7 +73,7 @@ sshConfigSetUp() {
     echo "PermitRootLogin prohibit-password" >> /etc/ssh/sshd_config.d/00-custom.conf
     echo "PermitEmptyPasswords no" >> /etc/ssh/sshd_config.d/00-custom.conf
 
-    sudo systemctl reload ssh
+    systemctl reload ssh
 }
 
 # Got through part of logging but I think this is gonna be complex
@@ -87,9 +87,9 @@ auditdSetUp() {
 
     # start auditd after installing and configuring
     if [[ $ID == "alpine" ]]; then
-        sudo rc-service auditd start
+        rc-service auditd start
     else
-        sudo systemctl start auditd
+        systemctl start auditd
     fi
 
     laurelSetUp
@@ -98,16 +98,16 @@ auditdSetUp() {
 installAuditd() {
     # Install tools and audits using distro specific package manager
     if [[ $ID == "debian" || $ID == "ubuntu" ]]; then
-        sudo apt install git clang libacl1-dev
-        sudo apt install -y auditd 
-        sudo systemctl enable auditd
+        apt install git clang libacl1-dev
+        apt install -y auditd 
+        systemctl enable auditd
     elif [[ $ID == "fedora" || $ID_LIKE == "fedora" || $ID == "centos" || $ID == "rocky" || $ID == "almalinux" ]]; then
-        sudo dnf install git clang libacl-devel
-        sudo dnf install audit
-        sudo systemctl enable auditd
+        dnf install git clang libacl-devel
+        dnf install audit
+        systemctl enable auditd
     elif [[ $ID == "alpine" ]]; then
-        sudo apk add git clang acl-dev
-        sudo apk add audit && rc-update add auditd
+        apk add git clang acl-dev
+        apk add audit && rc-update add auditd
     fi
 }
 
@@ -115,38 +115,38 @@ configureAuditdRules() {
     wget https://raw.githubusercontent.com/Neo23x0/auditd/refs/heads/master/audit.rules -O audit.rules
 
     # Check this reg ex, it should find max_log_file line irregardless of spaces around equal sight (\s*) and current value ([0-9]\+)
-    sudo sed -i "s/^max_log_file\s*=\s*[0-9]\+/max_log_file=100/" audit.rules
+    sed -i "s/^max_log_file\s*=\s*[0-9]\+/max_log_file=100/" audit.rules
     # Comment of this line by putting # in front
-    sudo sed -i "s/^-a always,exclude -F msgtype=CWD/# -a always,exclude -F msgtype=CWD" audit.rules
+    sed -i "s/^-a always,exclude -F msgtype=CWD/# -a always,exclude -F msgtype=CWD" audit.rules
     echo "-a exit,always -S execve -k task" >> audit.rules
 
     # Copy configured Neo rules to master audit.rules file
-    sudo cp ./audit.rules /etc/audit/rules.d/audit.rules
+    cp ./audit.rules /etc/audit/rules.d/audit.rules
 
-    sudo augenrules
+    augenrules
 }
 
 laurelSetUp() {
     # Setting up Laurel for auditd
     git clone https://github.com/threathunters-io/laurel.git
     cd laurel
-    
-    sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
     source ~/.bashrc
     cargo build --release
-    sudo install -m755 target/release/laurel /usr/local/sbin/laurel
+    install -m755 target/release/laurel /usr/local/sbin/laurel
 
-    sudo useradd --system --home-dir /var/log/laurel --create-home _laurel
+    useradd --system --home-dir /var/log/laurel --create-home _laurel
 
     wget https://raw.githubusercontent.com/threathunters-io/laurel/refs/heads/master/etc/laurel/config.toml -O laurelConfig
     wget https://raw.githubusercontent.com/threathunters-io/laurel/refs/heads/master/etc/audit/plugins.d/laurel.conf -O laurelPlugin
 
-    sudo cp ./laurelConfig /etc/laurel/config.toml
+    cp ./laurelConfig /etc/laurel/config.toml
 
     if [[ -d /etc/audit/plugins.d/laurel.conf ]]; then
-        sudo cp ./laurelPlugin /etc/audit/plugins.d/laurel.conf
+        cp ./laurelPlugin /etc/audit/plugins.d/laurel.conf
     else
-        sudo cp ./laurelPlugin /etc/audisp/plugins.d/laurel.conf
+        cp ./laurelPlugin /etc/audisp/plugins.d/laurel.conf
     fi
 }
 
