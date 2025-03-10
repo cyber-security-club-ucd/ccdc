a37c1c03a2708d9e6960cc30b870143ed7a31224
Tyler C <taculp@ucdavis.edu>
2025-03-09 21:46:39 -0700

    Reverted to using sudo throughout the script

--- linux/sop/linux_sop.sh
+++ linux/sop/linux_sop.sh
@@ -13,19 +13,19 @@ installTools() {
 
     # Install tools and audits using distro specific package manager
     if [[ $ID == "debian" || $ID == "ubuntu" ]]; then
-        apt-get update
-        apt-get install git clang libacl1-dev vim nmap iproute2 curl
+        sudo apt-get update
+        sudo apt-get install -y git clang libacl1-dev vim nmap iproute2 curl
     elif [[ $ID == "fedora" || $ID_LIKE == "fedora" || $ID == "centos" || $ID == "rocky" || $ID == "almalinux" ]]; then
         if command -v dnf &>/dev/null; then
-            dnf update -y
-            dnf install -y git clang libacl-devel vim nmap iproute2 curl
+            sudo dnf update -y
+            sudo dnf install -y git clang libacl-devel vim nmap iproute2 curl
         elif command -v yum &>/dev/null; then
-            yum update -y
-            yum install -y git clang libacl-devel vim nmap iproute2 curl
+            sudo yum update -y
+            sudo yum install -y git clang libacl-devel vim nmap iproute2 curl
         fi
     elif [[ $ID == "alpine" ]]; then
-        apk update
-        apk add git clang acl-dev vim nmap iproute2 curl
+        sudo apk update
+        sudo apk add git clang acl-dev vim nmap iproute2 curl
     fi
 
     return 0
@@ -70,13 +70,13 @@ getMachineInfo() {
 getRunningServices() {
     mkdir -p $HOME/sop/running
 
-    systemctl --type=service --state=running >> $HOME/sop/running/runningServices.txt
-    systemctl list-unit-files --state=enabled >> $HOME/sop/running/enabledServices.txt
+    sudo systemctl --type=service --state=running >> $HOME/sop/running/runningServices.txt
+    sudo systemctl list-unit-files --state=enabled >> $HOME/sop/running/enabledServices.txt
 
-    ss -plnt >> $HOME/sop/running/openPorts.txt
-    ss -plnu >> $HOME/sop/running/openPorts.txt
+    sudo ss -plnt >> $HOME/sop/running/openPorts.txt
+    sudo ss -plnu >> $HOME/sop/running/openPorts.txt
 
-    nmap -p- localhost -On $HOME/sop/running/localNmapScan.txt
+    sudo nmap -p- localhost -On $HOME/sop/running/localNmapScan.txt
 }
 
 sshConfigSetUp() {
@@ -87,13 +87,13 @@ sshConfigSetUp() {
 
     # Make ssh_config.d if it doesn't exist already
     if [[ ! -d /etc/ssh/ssh_config.d ]]; then
-        mkdir -p /etc/ssh/ssh_config.d
+        sudo mkdir -p /etc/ssh/ssh_config.d
     fi
 
-    echo "PermitRootLogin prohibit-password" >> /etc/ssh/sshd_config.d/00-custom.conf
-    echo "PermitEmptyPasswords no" >> /etc/ssh/sshd_config.d/00-custom.conf
+    sudo echo "PermitRootLogin prohibit-password" >> /etc/ssh/sshd_config.d/00-custom.conf
+    sudo echo "PermitEmptyPasswords no" >> /etc/ssh/sshd_config.d/00-custom.conf
 
-    systemctl reload ssh
+    sudo systemctl reload ssh
 }
 
 # Got through part of logging but I think this is gonna be complex
@@ -108,9 +108,9 @@ auditdSetUp() {
 
     # start auditd after installing and configuring
     if [[ $ID == "alpine" ]]; then
-        rc-service auditd start
+        sudo rc-service auditd start
     else
-        systemctl start auditd
+        sudo systemctl start auditd
     fi
 
     laurelSetUp
@@ -119,13 +119,13 @@ auditdSetUp() {
 installAuditd() {
     # Install tools and audits using distro specific package manager
     if [[ $ID == "debian" || $ID == "ubuntu" ]]; then
-        apt install -y auditd 
-        systemctl enable auditd
+        sudo apt install -y auditd 
+        sudo systemctl enable auditd
     elif [[ $ID == "fedora" || $ID_LIKE == "fedora" || $ID == "centos" || $ID == "rocky" || $ID == "almalinux" ]]; then
-        dnf install audit
-        systemctl enable auditd
+        sudo dnf install audit
+        sudo systemctl enable auditd
     elif [[ $ID == "alpine" ]]; then
-        apk add audit && rc-update add auditd
+        sudo apk add audit && sudo rc-update add auditd
     fi
 }
 
@@ -133,15 +133,15 @@ configureAuditdRules() {
     wget https://raw.githubusercontent.com/Neo23x0/auditd/refs/heads/master/audit.rules -O audit.rules
 
     # Check this reg ex, it should find max_log_file line irregardless of spaces around equal sight (\s*) and current value ([0-9]\+)
-    sed -E -i "s/^max_log_file\s*=\s*[0-9]\+/max_log_file=100/" audit.rules
+    sudo sed -E -i "s/^max_log_file\s*=\s*[0-9]\+/max_log_file=100/" audit.rules
     # Comment of this line by putting # in front
-    sed -i "s/^-a always,exclude -F msgtype=CWD/# -a always,exclude -F msgtype=CWD" audit.rules
+    sudo sed -i "s/^-a always,exclude -F msgtype=CWD/# -a always,exclude -F msgtype=CWD" audit.rules
     echo "-a exit,always -S execve -k task" >> audit.rules
 
     # Copy configured Neo rules to master audit.rules file
-    cp ./audit.rules /etc/audit/rules.d/audit.rules
+    sudo cp ./audit.rules /etc/audit/rules.d/audit.rules
 
-    augenrules
+    sudo augenrules
 }
 
 laurelSetUp() {
@@ -149,11 +149,11 @@ laurelSetUp() {
 
     # Rust already installed if folder exists
     if [[ ! -e /root/.rustup/settings.toml ]]; then
-        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sudo sh
         source $HOME/.bashrc
     fi
 
-    if git clone https://github.com/threathunters-io/laurel.git $HOME/sop/auditdRules; then
+    if git clone https://github.com/threathunters-io/laurel.git $HOME/sop/auditdRules/; then
         echo "Successfully cloned in laurel"
     else
         echo "git clone of laurel failed"
@@ -162,28 +162,23 @@ laurelSetUp() {
     cd $HOME/sop/auditdRules/laurel
 
     cargo build --release
-    install -m755 target/release/laurel /usr/local/sbin/laurel
+    sudo install -m755 target/release/laurel /usr/local/sbin/laurel
 
-    useradd --system --home-dir /var/log/laurel --create-home _laurel
+    sudo useradd --system --home-dir /var/log/laurel --create-home _laurel
 
     wget https://raw.githubusercontent.com/threathunters-io/laurel/refs/heads/master/etc/laurel/config.toml -O laurelConfig
     wget https://raw.githubusercontent.com/threathunters-io/laurel/refs/heads/master/etc/audit/plugins.d/laurel.conf -O laurelPlugin
 
-    cp ./laurelConfig /etc/laurel/config.toml
+    sudo cp ./laurelConfig /etc/laurel/config.toml
 
     if [[ -d /etc/audit/plugins.d/laurel.conf ]]; then
-        cp ./laurelPlugin /etc/audit/plugins.d/laurel.conf
+        sudo cp ./laurelPlugin /etc/audit/plugins.d/laurel.conf
     else
-        cp ./laurelPlugin /etc/audisp/plugins.d/laurel.conf
+        sudo cp ./laurelPlugin /etc/audisp/plugins.d/laurel.conf
     fi
 }
 
 main() {
-    if [[ "$EUID" -ne 0 ]]; then
-        echo "must be root to run this scripts"
-        return 1
-    fi
-
     installTools
 
     mkdir -p $HOME/sop
